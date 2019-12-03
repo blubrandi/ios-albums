0a230061f3d821e8f72dd68686362a65a9aaf426
blubrandi <blubrandi0@gmail.com>
2019-12-02 23:06:15 -0700

    In AlbumDetailTableViewController:  Created custom cell with add song fields and added uiview with album info textfields.

---
+++ Albums/Albums/SongTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Brandi on 12/2/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var songTitle: UITextField!
+    @IBOutlet weak var songDuration: UITextField!
+    @IBOutlet weak var addSong: UIButton!
+    
+    
+    @IBAction func addSongTapped(_ sender: Any) {
+    }
+    
+}
